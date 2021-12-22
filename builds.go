1d4d84078a8ea6f75d481cb6981c98451bec8b23
Rchardon <33094562+Rchardon@users.noreply.github.com>
2021-12-22 18:33:51 +0100

    Update builds.go

--- builds.go
+++ builds.go
@@ -12,33 +12,27 @@ var (
 		// Treasure Room items
 		// -------------------
 
-		// #1 - Cricket's Body
-		// Cain - The range down causes Cain to barely be able to hit anything
-		// Samson - ???
-		// Azazel - The Brimstone beam inherits the split shots but they are not very good
-		// Tainted Azazel - The Brimstone beam inherits the split shots but they are not very good
-		{"Cain", "Samson", "Azazel", "Tainted Azazel"},
-
+		{}, // #1 - Cricket's Body
 		{}, // #2 - Cricket's Head
 
 		// #3 - Dead Eye
 		// Azazel - The Brimstone beam prevents it from working
-		// Lilith - It is hard to be accurate when shots come from Incubus
 		// Keeper - It is hard to be accurate with triple shot
 		// Tainted Azazel - The Brimstone beam prevents it from working
-		// Tainted Keeper - It is hard to be accurate with triple shot
-		{"Azazel", "Lilith", "Keeper", "Tainted Azazel", "Tainted Keeper"},
+		// Tainted Keeper - It is hard to be accurate with quad shot
+		{"Azazel", "Keeper", "Tainted Azazel", "Tainted Keeper"},
 
 		// #4 - Death's Touch
 		// The Forgotten - The piercing shots do nothing for the bone club
 		{"The Forgotten"},
 
 		// #5 - Dr. Fetus
-		{},
+		// Tainted Forgotten - Very annoying to use with the skeleton
+		{"Tainted Forgotten"},
 
 		// #6 - Ipecac
-		// Azazel - The short-range brimstone causes self-damage
-		{"Azazel"},
+		// Tainted Eve - Can cause unavoidable damage if a clot is behind you or shoot at an obstacle near you
+		{"Tainted Eve"},
 
 		{}, // #7 - Magic Mushroom
 		{}, // #8 - Mom's Knife
@@ -52,7 +46,10 @@ var (
 		// Devil Room items
 		// ----------------
 
-		{}, // #14 - Brimstone
+		// #14 - Brimstone
+		// Tainted Lilith - Gello fires the brim very slowly and auto-fire is not always accurate
+		{"Tainted Lilith"},
+
 		{}, // #15 - Maw of the Void
 
 		// ----------------
@@ -61,40 +58,52 @@ var (
 
 		// #16 - Crown of Light
 		// Eve - Eve cannot use the razor with this start
-		{"Eve"},
+		// Tainted Magdalene - Crown is never full with the depleting hearts
+		// Tainted Eve - Crown can't be active with the clot mechanic
+		{"Eve", "Tainted Magdalene", "Tainted Eve"},
 
 		{}, // #17 - Sacred Heart
-		{}, // #18 - Spirit Sword
+
+		// #18 - Spirit Sword
+		// The Forgotten - No synergy with the bone
+		// Tainted Forgotten - No synergy with the bone
+		{"The Forgotten", "Tainted Forgotten"},
+
 		{}, // #19 - Revelation
 
 		// -----------------
 		// Secret Room items
 		// -----------------
 
-		{}, // #20 - Epic Fetus
+		// #20 - Epic Fetus
+		// Tainted Lilith - The target keeps moving and you can't control it, making it impossible to target enemies
+		{"Tainted Lilith"},
 
 		// ------------
 		// Custom items
 		// ------------
 
-		{}, // #21 - Sawblade
+		// #21 - Sawblade
+		// Bethany - Very complicated to play orbitals with her because she can't protect herself from losing the deal with soul hearts
+		// Tainted Lost - Very complicated to play orbitals because of one-time mantles
+		{"Bethany", "Tainted Lost"},
 
-		// ------
 		// Builds
-		// ------
-
 		{}, // #22 - 20/20 + The Inner Eye
 		{}, // #23 - Chocolate Milk + Steven
-		{}, // #24 - Godhead + Cupid's Arrow
-		{}, // #25 - Haemolacria + The Sad Onion
 
-		// #26 - Incubus + Incubus
-		// The Forgotten - The extra Incubus with bone clubs are not very helpful
-		{"The Forgotten"},
+		// #24 - Godhead + Cupid's Arrow
+		// Azazel - Small damage up for a tears down, resulting in a lost of DPS overall
+		// The Forgotten - Does nothing on the bone club
+		// Tainted Forgotten - Small damage up for a tears down, resulting in a lost of DPS overall
+		{"Azazel", "The Forgotten", "Tainted Forgotten"},
+
+		{}, // #25 - Haemolacria + The Sad Onion
+		{}, // #26 - Incubus + Incubus
 
 		// #27 - Monstro's Lung + The Sad Onion
-		// Keeper - The charge rate is too low to be very useful
-		// Tainted Keeper - The charge rate is too low to be very useful
+		// Keeper - Huge tears down, resulting in a lost of DPS overall
+		// Tainted Keeper - Huge tears down, resulting in a lost of DPS overall
 		{"Keeper", "Tainted Keeper"},
 
 		{}, // #28 - Technology + A Lump of Coal
@@ -105,13 +114,13 @@ var (
 		// Azazel - The synergy is only useful with a tear build
 		// The Forgotten - The synergy is only useful with a tear build
 		// Tainted Azazel - The synergy is only useful with a tear build
-		{"Azazel", "The Forgotten", "Tainted Azazel"},
+		// Tainted Forgotten - The synergy is only useful with a tear build
+		{"Azazel", "The Forgotten", "Tainted Azazel", "Tainted Forgotten"},
 
 		// #32 - Eye of the Occult + Loki's Horns + 15 Luck
-		// Azazel - The synergy is only useful with a tear build
-		// The Forgotten - The synergy is only useful with a tear build
-		// Tainted Azazel - The synergy is only useful with a tear build
-		{"Azazel", "The Forgotten", "Tainted Azazel"},
+		// The Forgotten - It's only a damage up on the bone club
+		// Tainted Forgotten - It's only a damage up on the bone club
+		{"The Forgotten", "Tainted Forgotten"},
 
 		{}, // #33 - Distant Admiration + Friend Zone + Forever Alone + BFFS!
 	}
