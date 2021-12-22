338e09703d4a9acfeb0743fa1c3148f7bc9c1f82
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-22 11:29:57 -0500

    updating build exceptions

--- builds.go
+++ builds.go
@@ -8,49 +8,111 @@ var (
 	buildExceptions = [][]string{
 		{}, // #0 - n/a
 
+		// -------------------
 		// Treasure Room items
-		{"Cain", "Samson", "Azazel", "Tainted Azazel"}, // #1 - Cricket's Body
-		{}, // #2 - Cricket's Head
-		{"Azazel", "Lilith", "Keeper", "Tainted Azazel", "Tainted Keeper"}, // #3 - Dead Eye
-		{"The Forgotten"}, // #4 - Death's Touch
-		{},                // #5 - Dr. Fetus
-		{"Azazel"},        // #6 - Ipecac
-		{},                // #7 - Magic Mushroom
-		{},                // #8 - Mom's Knife
-		{},                // #9 - Polyphemus
-		{},                // #10 - Proptosis
-		{},                // #11 - Tech.5
-		{},                // #12 - Tech X
-		{},                // #13 - C Section
+		// -------------------
 
+		// #1 - Cricket's Body
+		// Cain - The range down causes Cain to barely be able to hit anything
+		// Samson - ???
+		// Azazel - The Brimstone beam inherits the split shots but they are not very good
+		// Tainted Azazel - The Brimstone beam inherits the split shots but they are not very good
+		{"Cain", "Samson", "Azazel", "Tainted Azazel"},
+
+		{}, // #2 - Cricket's Head
+
+		// #3 - Dead Eye
+		// Azazel - The Brimstone beam prevents it from working
+		// Lilith - It is hard to be accurate when shots come from Incubus
+		// Keeper - It is hard to be accurate with triple shot
+		// Tainted Azazel - The Brimstone beam prevents it from working
+		// Tainted Keeper - It is hard to be accurate with triple shot
+		{"Azazel", "Lilith", "Keeper", "Tainted Azazel", "Tainted Keeper"},
+
+		// #4 - Death's Touch
+		// The Forgotten - The piercing shots do nothing for the bone club
+		{"The Forgotten"},
+
+		// #5 - Dr. Fetus
+		{},
+
+		// #6 - Ipecac
+		// Azazel - The short-range brimstone causes self-damage
+		{"Azazel"},
+
+		{}, // #7 - Magic Mushroom
+		{}, // #8 - Mom's Knife
+		{}, // #9 - Polyphemus
+		{}, // #10 - Proptosis
+		{}, // #11 - Tech.5
+		{}, // #12 - Tech X
+		{}, // #13 - C Section
+
+		// ----------------
 		// Devil Room items
+		// ----------------
+
 		{}, // #14 - Brimstone
 		{}, // #15 - Maw of the Void
 
+		// ----------------
 		// Angel Room items
-		{"Eve"}, // #16 - Crown of Light
-		{},      // #17 - Sacred Heart
-		{},      // #18 - Spirit Sword
-		{},      // #19 - Revelation
+		// ----------------
 
+		// #16 - Crown of Light
+		// Eve - Eve cannot use the razor with this start
+		{"Eve"},
+
+		{}, // #17 - Sacred Heart
+		{}, // #18 - Spirit Sword
+		{}, // #19 - Revelation
+
+		// -----------------
 		// Secret Room items
+		// -----------------
+
 		{}, // #20 - Epic Fetus
 
+		// ------------
 		// Custom items
+		// ------------
+
 		{}, // #21 - Sawblade
 
+		// ------
 		// Builds
-		{"Azazel", "Keeper", "Tainted Keeper"}, // #22 - 20/20 + The Inner Eye
-		{},                                     // #23 - Chocolate Milk + Steven
-		{},                                     // #24 - Godhead + Cupid's Arrow
-		{},                                     // #25 - Haemolacria + The Sad Onion
-		{"The Forgotten"},                      // #26 - Incubus + Incubus
-		{"Keeper", "Tainted Keeper"},           // #27 - Monstro's Lung + The Sad Onion
-		{},                                     // #28 - Technology + A Lump of Coal
-		{},                                     // #29 - Twisted Pair + Twisted Pair
-		{},                                     // #30 - Pointy Rib + Eve's Mascara
-		{"Azazel", "Keeper", "The Forgotten", "Tainted Azazel", "Tainted Keeper"}, // #31 - Fire Mind + Mysterious Liquid + 13 Luck
-		{"Azazel", "Keeper", "The Forgotten", "Tainted Azazel", "Tainted Keeper"}, // #32 - Eye of the Occult + Loki's Horns + 15 Luck
+		// ------
+
+		{}, // #22 - 20/20 + The Inner Eye
+		{}, // #23 - Chocolate Milk + Steven
+		{}, // #24 - Godhead + Cupid's Arrow
+		{}, // #25 - Haemolacria + The Sad Onion
+
+		// #26 - Incubus + Incubus
+		// The Forgotten - The extra Incubus with bone clubs are not very helpful
+		{"The Forgotten"},
+
+		// #27 - Monstro's Lung + The Sad Onion
+		// Keeper - The charge rate is too low to be very useful
+		// Tainted Keeper - The charge rate is too low to be very useful
+		{"Keeper", "Tainted Keeper"},
+
+		{}, // #28 - Technology + A Lump of Coal
+		{}, // #29 - Twisted Pair + Twisted Pair
+		{}, // #30 - Pointy Rib + Eve's Mascara
+
+		// #31 - Fire Mind + Mysterious Liquid + 13 Luck
+		// Azazel - The synergy is only useful with a tear build
+		// The Forgotten - The synergy is only useful with a tear build
+		// Tainted Azazel - The synergy is only useful with a tear build
+		{"Azazel", "The Forgotten", "Tainted Azazel"},
+
+		// #32 - Eye of the Occult + Loki's Horns + 15 Luck
+		// Azazel - The synergy is only useful with a tear build
+		// The Forgotten - The synergy is only useful with a tear build
+		// Tainted Azazel - The synergy is only useful with a tear build
+		{"Azazel", "The Forgotten", "Tainted Azazel"},
+
 		{}, // #33 - Distant Admiration + Friend Zone + Forever Alone + BFFS!
 	}
 )
